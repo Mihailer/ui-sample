4bd81c0ebb460e5cb9137bf2cadccc7f1c4f6072
Mihailer <node.q13@gmail.com>
2023-01-25 01:38:33 +0300

    added: ui ButtonCmp

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
-  hem
+  <div>
+    <button-cmp btnName="Найти заказ"
+      :loading="false"
+      @click-handler="clickHandler" />
+  </div>
 </template>
 
 <script lang="ts">
-  export default {}
+import ButtonCmp from './components/ui/ButtonCmp.vue';
+
+  export default {
+    components: {
+      ButtonCmp
+    },
+
+    setup() {
+      const clickHandler = (): void => {
+        console.log( 'clickHandler');
+      }
+
+      return {
+        clickHandler
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  // color: #2c3e50;
+  // margin-top: 60px;
 }
 </style>
---
+++ src/components/ui/ButtonCmp.vue
@@ -0,0 +1,73 @@
+<template>
+    <button :class="[ loading ? 'load-class' : 'button-cmp' ]" 
+        type="button"
+        @click="load" >
+        {{ btnNameText }}
+        <spinner-cmp v-if="loading" />
+    </button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+import  SpinnerCmp  from './SpinnerCmp.vue'
+
+export default {
+    props: {
+        loading: { type: Boolean, default: true },
+        btnName: { type: String, default: 'Btn' }
+    },
+
+    emits: [ 'click-handler' ],
+
+    components: {
+        SpinnerCmp
+    },
+
+    setup( props: any, { emit }: any ) {
+        
+        const btnNameText = ref( props.btnName )
+        const buttonSize = ref( btnNameText.value.split('').length )
+        const loading = ref( props.loading )
+        
+        const load = (): void => {
+            
+            emit( 'click-handler' )
+            btnNameText.value = ''
+            loading.value = true  
+
+            setTimeout( (): void => {
+                loading.value = false 
+                btnNameText.value = props.btnName
+            }, 3000 )
+        }
+
+        return {
+            loading,
+            load,
+            btnNameText,
+            buttonSize
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import '~/src/assets/styles/custom.scss';
+
+.button-cmp {
+    @include flexRow( center, center );
+    @include font( 13px, normal );
+    height: 30px;
+    width: calc( 10px * v-bind( buttonSize ) );
+    border: $main-border;
+    border-radius: $main-border-radius;
+    outline: none;
+    color: $main-color;
+
+    &:hover {
+        background-color: $main-hover;
+        color: white;
+    }
+}
+</style>
